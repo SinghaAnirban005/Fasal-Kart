3b4c2eae95ad5351523c6b8813302683a62139e4
Anirban Singha <143536290+SinghaAnirban005@users.noreply.github.com>
2024-09-30 14:49:41 +0000

    Introduced models for Producer-Consumer

---
+++ Backend/src/models/Consumer.model.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose"
+import { Schema } from "mongoose"
+
+const consumerSchema = new Schema({
+    
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+
+    password: {
+        type: String,
+        required: [true, "Please enter password"],
+        trim: true,
+        unique: true,
+        minLength: 6,
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true,
+        index: true
+    },
+
+    refreshToken: {
+        type: String,
+    },
+
+    Cart: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: ""
+        }
+    ],
+
+    phoneNumber: {
+        type: Number,
+        required: true,
+        index: true,
+        unique: true
+    },
+
+    itemsBought: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: ''
+        }
+    ],
+
+}, 
+
+{
+    timestamps: true
+})
+
+
+export const Consumer = mongoose.model("Consumer", consumerSchema)
---
+++ Backend/src/models/Producer.model.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose"
+import { Schema } from "mongoose"
+
+const producerSchema = new Schema({
+    
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    
+    password: {
+        type: String,
+        required: [true, "Please enter password"],
+        trim: true,
+        unique: true,
+        minLength: 6,
+    },
+
+    province: [
+        {
+            type: String,
+        }
+    ],
+
+    phoneNumber: {
+        type: Number,
+        required: true,
+        index: true,
+        unique: true
+    },
+
+    Language: {
+            type: [String],
+            required: true,
+            default: ["English"],
+    },
+
+    Stock: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: '',
+        }
+    ],
+
+    refreshToken: {
+        type: String,
+    },
+
+    // Considering the case that each farmer would get reels based on their location 
+    Reels: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: '',
+        }
+    ],
+    
+}, 
+
+{
+    timestamps: true
+})
+
+
+export const Producer = mongoose.model("Producer", producerSchema)
